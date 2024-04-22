47ecc324c503fe304ab5534b6c3c77641174671f
Koheya <mohamedsaeed4090@gmail.com>
2024-04-22 18:20:16 +0200

    Add inactive and active drawer item widgets
    Added InActiveDrawerItem and ActiveDrawerItem widgets to display inactive
    and active drawer items respectively. Also updated CustomDrawer to include
    these new widgets in the drawer items list view.

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/active_and_inactive_item.dart';
+import 'package:responsive_dash_board/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dash_board/widgets/user_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -9,13 +12,38 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          UserListTile(
-            image: Assets.imagesAvatar3,
-            title: "Lekan Okeowo",
-            subTitle: "demo@gmail.com",
-          )
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserListTile(
+              image: Assets.imagesAvatar3,
+              title: "Lekan Okeowo",
+              subTitle: "demo@gmail.com",
+            ),
+          ),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(
+                    child: SizedBox(
+                  height: 20,
+                )),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: "Settings System",
+                      image: Assets.imagesSettingSystem),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: "Logout Account",
+                      image: Assets.imagesLogoutAccount),
+                ),
+                SizedBox(height: 48),
+              ],
+            ),
+          ),
         ],
       ),
     );
